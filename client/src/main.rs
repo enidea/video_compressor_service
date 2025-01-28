60d9af1633c37dad54dd139e1d3afd7e9b748579
eriku66 <eriku.hashimoto@gmail.com>
2025-01-28 23:44:10 +0900

    feat: 🎸 implement basic TCP communication

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use std::{io::Write, net::TcpStream};
+
+use anyhow::Ok;
+
+fn main() -> anyhow::Result<()> {
+    let mut tcp_stream = TcpStream::connect(shared::SERVER_ADDR)?;
+
+    tcp_stream.write_all(b"Hello, server!")?;
+
+    Ok(())
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+fn main() -> anyhow::Result<()> {
+    let tcp_listener = TcpListener::bind(shared::SERVER_ADDR)?;
+
+    for tcp_stream in tcp_listener.incoming() {
+        match tcp_stream {
+            Ok(tcp_stream) => {
+                println!("Accepted connection from: {}", tcp_stream.peer_addr()?);
+            }
+            Err(e) => {
+                eprintln!("Error accepting connection: {}", e);
+            }
+        }
+    }
+
+    Ok(())
 }
