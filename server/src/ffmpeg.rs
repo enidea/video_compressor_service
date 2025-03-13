4088c1afa63eecda43d08618cecb56339e37a115
eriku66 <eriku.hashimoto@gmail.com>
2025-03-14 00:28:16 +0900

    feat: 🎸 add support for custom output resolution in ffmpeg conversion

--- server/src/ffmpeg.rs
+++ server/src/ffmpeg.rs
@@ -3,12 +3,30 @@ mod options;
 use options::Options;
 pub use options::OptionsBuilder;
 
-use std::path::Path;
+use std::{path::Path, process::Command};
 
 pub fn convert(
     input_file_path: &Path,
     output_file_path: &Path,
     options: Options,
 ) -> anyhow::Result<()> {
+    let mut args = vec![
+        String::from("-i"),
+        input_file_path.to_str().unwrap().to_string(),
+    ];
+
+    if let (Some(width), Some(height)) = (options.width, options.height) {
+        args.push(String::from("-s"));
+        args.push(format!("{}x{}", width, height));
+    }
+
+    args.push(output_file_path.to_str().unwrap().to_string());
+
+    let status = Command::new("ffmpeg").args(args).status()?;
+
+    if !status.success() {
+        return Err(anyhow::anyhow!("Failed to convert the file"));
+    }
+
     Ok(())
 }
